de3c349e4836ddaebabe56bc75f750d7c25a1c12
najmiddinabdullaev <najmiddin.9898@mail.ru>
2022-08-31 18:29:12 +0500

    Upgrade js and css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const  modifiers = {
+  tabItemActive: `tabs__item--active`,
+  tabPanelsItemActive: `tabpanels__item--active`,
+  tabAccordionItemOpen: `accordion__item--open`
+};
+
 const elsTabsItem = document.querySelectorAll(`.tabs__item`);
 const elsTabsPanel = document.querySelectorAll(`.tabpanels__item`);
 const elsTabLink = document.querySelectorAll(`.js-tab-link`);
@@ -8,19 +14,19 @@ const elsAccordionItem = document.querySelectorAll(`.accordion__item`)
 
 function deactivateTabItems () {
   elsTabsItem.forEach(function (elTabsItem) {
-    elTabsItem.classList.remove(`tabs__item--active`);
+    elTabsItem.classList.remove(modifiers.tabItemActive);
   });
 }
 
 function deactivateTabPanels () {
   elsTabsPanel.forEach(function (elTabsPanel) {
-    elTabsPanel.classList.remove(`tabpanels__item--active`);
+    elTabsPanel.classList.remove(modifiers.tabPanelsItemActive);
   });
 }
 
 function closeAccordionItems () {
   elsAccordionItem.forEach(function (elAccordionItem) {
-    elAccordionItem.classList.remove(`accordion__item--open`);
+    elAccordionItem.classList.remove(modifiers.tabAccordionItemOpen);
   });
 }
 
@@ -33,7 +39,7 @@ elsTabLink.forEach(function (elTabLink) {
     deactivateTabItems ();
 
     // Add active class to current tabs__item
-    elTabLink.parentElement.classList.add(`tabs__item--active`);
+    elTabLink.parentElement.classList.add(modifiers.tabItemActive);
 
     // Remove active class from tabs__item elements
     deactivateTabPanels ();
@@ -41,7 +47,7 @@ elsTabLink.forEach(function (elTabLink) {
     // Show active tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split(`#`)[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add(`tabpanels__item--active`);
+    elTargetPanel.classList.add(modifiers.tabPanelsItemActive);
   });
 });
 
@@ -50,6 +56,6 @@ elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
   elAccordionItemToggler.addEventListener(`click`, function () {
     closeAccordionItems();
 
-    elAccordionItemToggler.closest(`.accordion__item`).classList.add(`accordion__item--open`);
+    elAccordionItemToggler.closest(`.accordion__item`).classList.add(modifiers.tabAccordionItemOpen);
   });
 });
